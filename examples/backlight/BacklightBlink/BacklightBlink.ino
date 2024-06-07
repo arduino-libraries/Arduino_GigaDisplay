78097537b37dea892d8aee503e9b1d1934701026
Ali Jahangiri <75624145+aliphys@users.noreply.github.com>
2024-06-07 16:42:36 +0200

    docs: add header comment

--- examples/backlight/BacklightBlink/BacklightBlink.ino
+++ examples/backlight/BacklightBlink/BacklightBlink.ino
@@ -1,7 +1,12 @@
 /*
-  Changes the display backlight from very dim to 100%
-  This example code is in the public domain.
-*/
+ * This example shows how to change the backlight intensity of the GIGA Display Shield.
+ * 
+ * The backlight intensity is set by passing a number between 0 and 100 to the .set() method.
+ * Here we set the backlight to 0% and 100% intensity in a loop, changing the value every 1 second.
+ * 
+ * Initial author: Martino Facchin @facchinm
+ * Modified by: Ali Jahangiri @aliphys
+ */
 
 #include <Arduino_GigaDisplay.h>
 
@@ -9,7 +14,6 @@
 GigaDisplayBacklight backlight;
 
 void setup() {
-  //init library
   backlight.begin();
 }
 
