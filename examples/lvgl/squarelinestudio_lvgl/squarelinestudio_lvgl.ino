1a903443a8c3ca072cffb3cf02a4d7be14edc68d
Ali Jahangiri <75624145+aliphys@users.noreply.github.com>
2024-06-10 15:37:12 +0200

    Update examples/lvgl/squarelinestudio_lvgl/squarelinestudio_lvgl.ino
    Co-authored-by: Leonardo Cavagnis <45899760+leonardocavagnis@users.noreply.github.com>

--- examples/lvgl/squarelinestudio_lvgl/squarelinestudio_lvgl.ino
+++ examples/lvgl/squarelinestudio_lvgl/squarelinestudio_lvgl.ino
@@ -8,7 +8,7 @@
  * 1. Create a SquareLine Studio project with the following settings:
  *    - Resolution: 800x480
  *    - Color depth: 16-bit
- *    - LVGL version: 9.1
+ *    - LVGL version: 9.x.x
  * 2. Design your GUI using the drag-and-drop tool.
  * 3. Export the LVGL UI files.
  * 4. Open the exported file and copy the 'libraries/ui' folder into your 'Arduino/libraries' directory.
